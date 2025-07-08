1778c1e22860f92f0a6301c98cc37bf98cf64efc
pabloqpacin <110383181+pabloqpacin@users.noreply.github.com>
2025-07-09 00:27:16 +0200

    refactor: move hello_world.go to root directory

--- hello_world.go
+++ hello_world.go
@@ -2,7 +2,8 @@
 > [@Fireship: Go in 100 Seconds](https://www.youtube.com/watch?v=446E-r0rXHI)
 
 To compile and run program:
-$ go build hello_world.go
+$ go run hw.go      # executable binary in memory
+$ go build wg.go    # executable in same dir as src file
 */
 
 package main
