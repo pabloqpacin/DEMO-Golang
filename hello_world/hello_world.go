a056b3fa7c178f23e48cde36027e60205379efc6
pabloqpacin <110383181+pabloqpacin@users.noreply.github.com>
2025-07-09 00:05:12 +0200

    feat(hello_world.go): add simple self-documenting program

---
+++ hello_world/hello_world.go
@@ -0,0 +1,14 @@
+/*
+> [@Fireship: Go in 100 Seconds](https://www.youtube.com/watch?v=446E-r0rXHI)
+
+To compile and run program:
+$ go build hello_world.go
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World!")
+}
