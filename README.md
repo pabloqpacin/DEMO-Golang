6d83e69b447163ac195304fe8d8802212fb274a8
pabloqpacin <110383181+pabloqpacin@users.noreply.github.com>
2025-07-20 19:17:57 +0200

    docs(LGWT): create LGWT/docs/ proper

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # DEMO-Golang
 
 1. [Documentation](/docs)
-2. [Learn Go with Tests](/Learn_Go_with_tests/)
+2. [Learn Go With Tests](/Learn_Go_With_Tests/)
 
 
--- hello_world.go
+++ hello_world.go
@@ -2,8 +2,8 @@
 > [@Fireship: Go in 100 Seconds](https://www.youtube.com/watch?v=446E-r0rXHI)
 
 To compile and run program:
-$ go run hw.go      # executable binary in memory
-$ go build wg.go    # executable in same dir as src file
+$ go run hello_world.go      # executable binary in memory
+$ go build hello_world.go    # executable in same dir as src file
 */
 
 package main
